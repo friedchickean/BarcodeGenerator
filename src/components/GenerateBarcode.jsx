8763e51c81069897273779258b178995c0716192
friedchickean <kdarg29@gmail.com>
2024-05-01 01:42:44 -0600

    responsive fixes

--- src/components/GenerateBarcode.jsx
+++ src/components/GenerateBarcode.jsx
@@ -23,18 +23,30 @@ const GenerateBarcode = () => {
             </div>
             <div className='flex flex-col p-5 gap-5 w-full flex-grow'>
 
-                <div className='shadow-md rounded-md flex flex-col md:flex-row p-5 bg-white justify-between items gap-10 items-center'>
-                    <div className="flex flex-col w-3/5">
+                <div className='shadow-md rounded-md flex flex-col md:flex-row p-5 bg-white justify-between items gap-2 md:gap-10 items-center'>
+                    <div className="flex flex-col w-full md:w-3/5 gap-2">
                         <h1 className='font-medium text-xl'>Barcode</h1>
-                        <span className='p-2 border-2 rounded-md border-gray-400 text-lg tracking-wide my-2 min-h-12'>{barcode}</span>
+                        <span className='p-2 border-2 rounded-md border-gray-400 text-lg tracking-wide min-h-12'>{barcode}</span>
                     </div>
-                    <button className='bg-indigo-700 w-1/5 h-2/3 text-white p-2 transition-all hover:bg-indigo-800 rounded-md text-lg hover:font-semibold' onClick={() => createBarcode()}>Generate Barcode</button>
-                    <button className='bg-white-700 border-2 w-1/5 h-2/3 p-2 rounded-md text-lg transition-all hover:bg-gray-100 hover:font-semibold' onClick={() => setBarcode('')}>Clear Barcode</button>
+                    <div className="flex flex-col w-full md:w-2/5 gap-2">
+                        <h1 className='font-medium text-xl'>Actions</h1>
+                        <div className="flex gap-5 items-center justify-center">
+                            <button className='bg-indigo-700 w-1/2 text-white p-2 transition-all hover:bg-indigo-800 rounded-md text-lg hover:font-semibold' onClick={() => createBarcode()}>Generate Barcode</button>
+                            <button className='bg-white-700 border-2 w-1/2 p-2 rounded-md text-lg transition-all hover:bg-gray-100 hover:font-semibold' onClick={() => setBarcode('')}>Clear Barcode</button>
+                        </div>
+                    </div>
                 </div>
 
                 <div className='shadow-md rounded-md w-full h-full flex flex-col p-5 bg-white justify-center items-center'>
                     <div ref={componentRef} className={`${barcode ? 'border-2' : ''} rounded-md p-5 min-w-80 min-h-58 justify-center items-center flex`}>
-                        {barcode ? <Barcode value={barcode} width={'2.3'} /> : <span>No barcode</span>}
+                        {barcode ? (<>
+                            <div className="block sm:hidden">
+                                <Barcode value={barcode} width={'1.5'} />
+                            </div>
+                            <div className="hidden sm:block">
+                                <Barcode value={barcode} />
+                            </div>
+                        </>) : <span>No barcode</span>}
                     </div>
                     <ReactToPrint
                         trigger={() => <button className='border-indigo-700 transition-all hover:bg-indigo-700 hover:text-white border-2 p-2 rounded-md w mt-5 flex items-center gap-2'>Print Barcode <FaPrint /></button>}
